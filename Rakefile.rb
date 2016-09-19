c4489e827ee54b08cd9faa132cf13759c7295b24
dogatana <dogatana@gmail.com>
2016-09-19 09:30:45 +0900

    校正処理を fix.rb へ分離

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,46 +1,21 @@
-file = 'Ruby環境構築講座Windows編-1.0.1.epub'
-#file = 'ruby.epub'
+require 'fix'
 
-def fix_chap1
-  name = 'OEBPS/chap1.html'
-  doc = ''
-  open(name, 'r:utf-8') { |f| doc = f.read }
-  doc.sub!(/をを/, 'を')
-  open(name, 'w:utf-8') { |f| f.write(doc) }
-end
-
-def fix_chap3
-  name = 'OEBPS/chap3.html'
-  doc = ''
-  open(name, 'r:utf-8') { |f| doc = f.read }
-  doc.sub!(/setupr\.b/, 'setup.rb')
-  open(name, 'w:utf-8') { |f| f.write(doc) }
-end
-
-def fix_opf
-  name = 'OEBPS/book.opf'
-  doc = ''
-  open(name, 'r:utf-8') { |f| doc = f.read }
-  %q("images/cover.jpg" media-type="image/jpeg")
-  doc.sub!(%r("images/cover.jpg" media-type="image/jpeg"), '\& properties="cimage"')
-  open(name, 'w:utf-8') { |f| f.write(doc) }
-end
-
+EPUB_MASTER = 'Ruby環境構築講座Windows編-1.0.1.epub'
 
 task :default => 'book.mobi'
 
 desc 'create mobi file'
-file 'book.mobi' => :patch do
-  sh 'kindlegen OEBPS/book.opf'
+file 'book.mobi' => :fix do
+  # sh 'kindlegen OEBPS/book.opf'
 end
 
-task :patch => 'OEBPS' do
-  fix_chap1
-  fix_chap3
-  fix_opf
+desc 'fix type and others'
+task :fix => 'OEBPS' do
+  Fix.fix('OEBPS')
+  # Fix.fix('OEBPS', 'OEBPS.patch')
 end
 
 desc 'extract OEBPS directory'
-file 'OEBPS' => file do
+file 'OEBPS' => EPUB_MASTER do
   sh "7z x #{file} OEBPS"
 end
